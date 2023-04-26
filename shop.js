744762e4a545d26aee6e0d48cedaff39d43ffeff
Zhuang <chngdolphin@gmail.com>
2023-04-27 02:38:56 +0800

    feat: add cart module & left channel list

--- shop.js
+++ shop.js
@@ -1,7 +1,8 @@
 window.onload = init
 
 function init() {
-    j_menu();
+    j_menu()
+    cartHoverListener()
 }
 
 // 下拉列表
@@ -12,11 +13,23 @@ function j_menu() {
     for (var i = 0; i < nav.length; i++) {
         nav[i].onmouseover = function () {
             var J_menu = document.querySelector('.header_nav_menu');
-            J_menu.style.display = 'block';
+            J_menu.style.height = '229px';
         }
-        nav[i].onmouseout = function () {
+        nav[i].onmouseleave = function () {
             var J_menu = document.querySelector('.header_nav_menu');
-            J_menu.style.display = 'none';
+            J_menu.style.height = '0px';
         }
     }
+}
+function cartHoverListener() {
+    let el1 = document.getElementById("J_miniCartBtn")
+    let el2 = document.getElementById("J_miniCartMenu")
+    el1.addEventListener("mouseover",function() {
+        el2.style.height = "100px"
+        el1.style.background = "#fff"
+    })
+    el1.addEventListener("mouseleave",function() {
+        el2.style.height = "0px"
+        el1.style.background = ""
+    })
 }
